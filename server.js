b0b08c1746ded04df741bb89c11fda8cb00e2408
Yacov yitzhak <yacov.mobile@gmail.com>
2020-09-28 21:26:46 +0300

    add ascci api

--- server.js
+++ server.js
@@ -3,7 +3,9 @@ const socket = require('socket.io');
 const app = express();
 const PORT =  process.env.PORT || 3000;
 const server = app.listen(PORT);
+const asciiAPI = require('./api/ascii');
 
+app.use('/api/ascii', asciiAPI);
 app.use(express.static('public'));
 
 const io = socket(server);
